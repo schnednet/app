5e389076750bc6730f48d35801084a3c869e4b43
schnednet <schnednet@schnednet-PC>
2015-10-27 19:00:52 +0100

    Share generierte OrganisationsChart Projekte

---
+++ plugins/org.bflow.toolbox.orgc.diagram/src/orgchart/diagram/preferences/DiagramConnectionsPreferencePage.java
@@ -0,0 +1,21 @@
+/*
+ * 
+ */
+package orgchart.diagram.preferences;
+
+import org.eclipse.gmf.runtime.diagram.ui.preferences.ConnectionsPreferencePage;
+import orgchart.diagram.part.OrgcDiagramEditorPlugin;
+
+/**
+ * @generated
+ */
+public class DiagramConnectionsPreferencePage extends ConnectionsPreferencePage {
+
+	/**
+	 * @generated
+	 */
+	public DiagramConnectionsPreferencePage() {
+		setPreferenceStore(OrgcDiagramEditorPlugin.getInstance()
+				.getPreferenceStore());
+	}
+}
