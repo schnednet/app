d0b22553b7cf29db771cdb0895ebfdccf20cb40e
schnednet <schnednet@schnednet-PC>
2015-12-20 11:11:10 +0100

    Alle KAnten werden jetzt rechtwinkelig geroutet

--- plugins/org.bflow.toolbox.vc.diagram/src/vcchart/diagram/preferences/DiagramConnectionsPreferencePage.java
+++ plugins/org.bflow.toolbox.vc.diagram/src/vcchart/diagram/preferences/DiagramConnectionsPreferencePage.java
@@ -4,6 +4,9 @@
 package vcchart.diagram.preferences;
 
 import org.eclipse.gmf.runtime.diagram.ui.preferences.ConnectionsPreferencePage;
+import org.eclipse.gmf.runtime.diagram.ui.preferences.IPreferenceConstants;
+import org.eclipse.gmf.runtime.notation.Routing;
+import org.eclipse.jface.preference.IPreferenceStore;
 
 import vcchart.diagram.part.VcDiagramEditorPlugin;
 
@@ -18,5 +21,17 @@ public class DiagramConnectionsPreferencePage extends ConnectionsPreferencePage 
 	public DiagramConnectionsPreferencePage() {
 		setPreferenceStore(VcDiagramEditorPlugin.getInstance()
 				.getPreferenceStore());
+	}
+	
+	/**
+	* Initializes the default preference values for this preference store.
+	* 
+	* @param preferenceStore
+	*            the preference store
+	*
+	* @generated NOT
+	*/
+	public static void initDefaults(IPreferenceStore preferenceStore) {
+	  preferenceStore.setDefault(IPreferenceConstants.PREF_LINE_STYLE, Routing.RECTILINEAR);
 	}
 }
